b689d1125ce733c6944d77ece5824784230330a6
Eli Cohen <eli.cohen.ws@gmail.com>
2022-08-25 08:58:17 +0300

    enter and error validation

--- src/components/post-input-card/PostInputCard.tsx
+++ src/components/post-input-card/PostInputCard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../redux/app/RootReducer';
 import { editPost, Post } from '../../redux/features/main/MainReducerSlice';
@@ -10,12 +10,15 @@ export default function PostInputCard({ selectedPost, setSelectedPost }: { selec
     const [title, setTitle] = useState(post ? post.title : '');
     const [body, setBody] = useState(post ? post.body : '');
     const dispatch = useDispatch();
+    const titleRef = useRef<any>(null);
+    const bodyRef = useRef<any>(null);
 
     useEffect(() => {
         if (selectedPost !== null) {
             setAnimationClass('animation-up');
             setTitle(post!.title);
             setBody(post!.body);
+            titleRef.current?.focus();
         } else {
             if (animationClass !== '') {
                 setAnimationClass('animation-down');
@@ -24,16 +27,43 @@ export default function PostInputCard({ selectedPost, setSelectedPost }: { selec
     }, [selectedPost, animationClass, post]);
 
     function handleSave() {
-        dispatch(editPost({ ...post!, title, body }));
-        setSelectedPost(null);
+        if (title.split(' ').join('') === '' || body.split(' ').join('') === '') {
+            alert('Title and Body cannot be empty!');
+        } else {
+            dispatch(editPost({ ...post!, title, body }));
+            setSelectedPost(null);
+        }
     };
+
+    function handleTitleKeyPress(event: any) {
+        if (event.key === 'Enter') {
+            bodyRef!.current.focus();
+        }
+    }
+    function handleBodyKeyPress(event: any) {
+        if (event.key === 'Enter') {
+            titleRef!.current.focus();
+        }
+    }
 
     return (
         <div>
             {animationClass === 'animation-up' && <div onClick={() => setSelectedPost(null)} className='blocking-frame'></div>}
             <div className={`post-input-card ${animationClass}`} >
-                <input className='post-input-title' placeholder='Title' value={title} onChange={(e) => setTitle(e.target.value)}></input>
-                <textarea className='post-input-body' placeholder='Body' value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+                <input
+                    className='post-input-title'
+                    ref={titleRef} placeholder='Title'
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    onKeyPress={handleTitleKeyPress}
+                ></input>
+                <textarea
+                    className='post-input-body'
+                    placeholder='Body'
+                    value={body} onChange={(e) => setBody(e.target.value)}
+                    ref={bodyRef}
+                    onKeyPress={handleBodyKeyPress}
+                ></textarea>
                 <div className='post-input-buttons'>
                     <input type="submit" value="Save" onClick={handleSave} />
                     <input type="submit" value="cancel" onClick={() => setSelectedPost(null)} />
